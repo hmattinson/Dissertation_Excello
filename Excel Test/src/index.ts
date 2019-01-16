d14de6b56d6e2f5d74522014d336c7dbb8dcae9a
hmattinson <henrymattinson@Westfieldhouse.org>
2019-01-16 12:29:39 +0000

    Created a Surge repo for the manifest to point to and edited manifest

--- Excel Test/src/index.ts
+++ Excel Test/src/index.ts
@@ -35,48 +35,48 @@ async function run() {
         Tone.context = new AudioContext();
         Tone.Transport.bpm.value = getBPM(sheet.values);
 
-        // piano = new Tone.Sampler({
-        //     'A0' : 'A0.[mp3|ogg]',
-        //     'C1' : 'C1.[mp3|ogg]',
-        //     'D#1' : 'Ds1.[mp3|ogg]',
-        //     'F#1' : 'Fs1.[mp3|ogg]',
-        //     'A1' : 'A1.[mp3|ogg]',
-        //     'C2' : 'C2.[mp3|ogg]',
-        //     'D#2' : 'Ds2.[mp3|ogg]',
-        //     'F#2' : 'Fs2.[mp3|ogg]',
-        //     'A2' : 'A2.[mp3|ogg]',
-        //     'C3' : 'C3.[mp3|ogg]',
-        //     'D#3' : 'Ds3.[mp3|ogg]',
-        //     'F#3' : 'Fs3.[mp3|ogg]',
-        //     'A3' : 'A3.[mp3|ogg]',
-        //     'C4' : 'C4.[mp3|ogg]',
-        //     'D#4' : 'Ds4.[mp3|ogg]',
-        //     'F#4' : 'Fs4.[mp3|ogg]',
-        //     'A4' : 'A4.[mp3|ogg]',
-        //     'C5' : 'C5.[mp3|ogg]',
-        //     'D#5' : 'Ds5.[mp3|ogg]',
-        //     'F#5' : 'Fs5.[mp3|ogg]',
-        //     'A5' : 'A5.[mp3|ogg]',
-        //     'C6' : 'C6.[mp3|ogg]',
-        //     'D#6' : 'Ds6.[mp3|ogg]',
-        //     'F#6' : 'Fs6.[mp3|ogg]',
-        //     'A6' : 'A6.[mp3|ogg]',
-        //     'C7' : 'C7.[mp3|ogg]',
-        //     'D#7' : 'Ds7.[mp3|ogg]',
-        //     'F#7' : 'Fs7.[mp3|ogg]',
-        //     'A7' : 'A7.[mp3|ogg]',
-        //     'C8' : 'C8.[mp3|ogg]'
-        //     }, {
-        //     'release' : 1,
-        //     'baseUrl' : '../assets/samples/salamander/',
-        //     'onload': function() {
-        //         runTurtles(sheet.values);
-        //         Tone.Transport.start("+0.1");
-        //     }
-        // }).toMaster();
+        piano = new Tone.Sampler({
+            'A0' : 'A0.[mp3|ogg]',
+            'C1' : 'C1.[mp3|ogg]',
+            'D#1' : 'Ds1.[mp3|ogg]',
+            'F#1' : 'Fs1.[mp3|ogg]',
+            'A1' : 'A1.[mp3|ogg]',
+            'C2' : 'C2.[mp3|ogg]',
+            'D#2' : 'Ds2.[mp3|ogg]',
+            'F#2' : 'Fs2.[mp3|ogg]',
+            'A2' : 'A2.[mp3|ogg]',
+            'C3' : 'C3.[mp3|ogg]',
+            'D#3' : 'Ds3.[mp3|ogg]',
+            'F#3' : 'Fs3.[mp3|ogg]',
+            'A3' : 'A3.[mp3|ogg]',
+            'C4' : 'C4.[mp3|ogg]',
+            'D#4' : 'Ds4.[mp3|ogg]',
+            'F#4' : 'Fs4.[mp3|ogg]',
+            'A4' : 'A4.[mp3|ogg]',
+            'C5' : 'C5.[mp3|ogg]',
+            'D#5' : 'Ds5.[mp3|ogg]',
+            'F#5' : 'Fs5.[mp3|ogg]',
+            'A5' : 'A5.[mp3|ogg]',
+            'C6' : 'C6.[mp3|ogg]',
+            'D#6' : 'Ds6.[mp3|ogg]',
+            'F#6' : 'Fs6.[mp3|ogg]',
+            'A6' : 'A6.[mp3|ogg]',
+            'C7' : 'C7.[mp3|ogg]',
+            'D#7' : 'Ds7.[mp3|ogg]',
+            'F#7' : 'Fs7.[mp3|ogg]',
+            'A7' : 'A7.[mp3|ogg]',
+            'C8' : 'C8.[mp3|ogg]'
+            }, {
+            'release' : 1,
+            'baseUrl' : '../assets/samples/salamander/',
+            'onload': function() {
+                runTurtles(sheet.values);
+                Tone.Transport.start("+0.1");
+            }
+        }).toMaster();
 
-        runTurtles(sheet.values);
-        Tone.Transport.start("+0.2");
+        // runTurtles(sheet.values);
+        // Tone.Transport.start("+0.2");
         
         /// console.log(`The range values "${selectedRange.values}".`);
     });
@@ -401,7 +401,7 @@ function playSequence(values: [string, number][], speedFactor: number =1, repeat
     console.log(noteTimes);
 
     var synthPart = new Tone.Part(function(time: string, note: [string, string, number]){
-        polySynth.triggerAttackRelease(note[0], note[1], time, note[2]);
+        piano.triggerAttackRelease(note[0], note[1], time, note[2]);
     }, noteTimes).start();
 
     if (repeats>0){
