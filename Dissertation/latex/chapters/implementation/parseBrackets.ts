80872afdc5a405e442f3562a13f1c59b187fda5f
hmattinson <henrymattinson@Westfieldhouse.org>
2019-05-08 22:22:33 +0100

    minor changes from Alan

--- Dissertation/latex/chapters/implementation/parseBrackets.ts
+++ Dissertation/latex/chapters/implementation/parseBrackets.ts
@@ -50,35 +50,3 @@ export function parseBrackets(str: string) {
 
 	return reNest(unnestedStr[0])
 }
-
-export function processParsedBrackets(arr) {
-	var s = "";
-	var wasPrevArray = false;
-	var prevArray = "";
-	for (let val of arr) {
-		if (val.constructor === Array) {
-			prevArray = processParsedBrackets(val)
-			wasPrevArray = true;
-		}
-		else {
-			var singleInstructions = val.trim().split(" ");
-			if (wasPrevArray) {
-				s = s + prevArray;
-				if (!isNaN(singleInstructions[0])) {
-					for (var i=1; i<singleInstructions[0]; i++) {
-						s = s + prevArray;
-					}
-					singleInstructions = singleInstructions.slice(1);
-				}
-			}
-			for (let instruction of singleInstructions) {
-				s = s + instruction + " ";
-			}
-			wasPrevArray = false;
-		}
-	}
-	if (wasPrevArray) {
-		s = s + prevArray;
-	}
-	return s;
-}
